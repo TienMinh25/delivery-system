c26e0a2081ba782f1ae57e4815c8ab936679096b
TienMinh25 <letienminh2512@gmail.com>
2025-02-19 15:01:27 +0700

    feat: done change password, delete user, forgot password, signout, update user. reset password, confirm change email and confirm sign up aren't be done

--- keycloak/keycloak.go
+++ keycloak/keycloak.go
@@ -27,8 +27,30 @@ func NewAuth() pkg.Auth {
 }
 
 // ChangePassword implements pkg.Auth.
-func (a *auth) ChangePassword(ctx context.Context, change *pkg.ChangePasswordRequest) error {
-	panic("unimplemented")
+func (a *auth) ChangePassword(ctx context.Context, payload *pkg.ChangePasswordRequest) error {
+	// login to check the old password
+	_, err := a.client.Login(ctx, a.clientID, a.clientSecret, a.realm, payload.Email, payload.OldPassword)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.Login")
+	}
+
+	// get admin token to set new password
+	adminToken, err := a.client.LoginClient(ctx, a.clientID, a.clientSecret, a.realm)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.LoginClient")
+	}
+
+	// password will be reset, so all released token should be revoked
+	err = a.client.LogoutAllSessions(ctx, adminToken.AccessToken, a.realm, payload.UserID)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.LogoutAllSessions")
+	}
+
+	// change password
+	return a.client.SetPassword(ctx, adminToken.AccessToken, payload.UserID, a.realm, payload.NewPassword, false)
 }
 
 // CheckToken implements pkg.Auth. (using for authorize)
@@ -48,23 +70,49 @@ func (a *auth) CheckToken(ctx context.Context, accessToken string) (*pkg.User, e
 
 // ConfirmChangeEmail implements pkg.Auth.
 func (a *auth) ConfirmChangeEmail(ctx context.Context, accessToken string, verifyEmail *pkg.VerifyEmailRequest) error {
-	panic("unimplemented")
+	// TODO: modify it
+	return nil
 }
 
 // ConfirmSignUp implements pkg.Auth.
 func (a *auth) ConfirmSignUp(ctx context.Context, verify *pkg.VerifyEmailRequest) error {
-	// TODO: do not understand this
+	// TODO: modify it
 	return nil
 }
 
 // DeleteUser implements pkg.Auth.
 func (a *auth) DeleteUser(ctx context.Context, userID string) error {
-	panic("unimplemented")
+	adminToken, err := a.client.LoginClient(ctx, a.clientID, a.clientSecret, a.realm)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.LoginClient")
+	}
+
+	// ! Do NOT delete users, just inactive them.
+	// a.client.DeleteUser(ctx, token.AccessToken, a.realm, userID)
+
+	// in here decide to use soft delete
+	// soft delete
+
+	return a.client.UpdateUser(ctx, adminToken.AccessToken, a.realm, gocloak.User{
+		ID:      gocloak.StringP(userID),
+		Enabled: gocloak.BoolP(false),
+	})
 }
 
 // ForgotPassword implements pkg.Auth.
 func (a *auth) ForgotPassword(ctx context.Context, userID string) error {
-	panic("unimplemented")
+	adminToken, err := a.client.LoginClient(ctx, a.clientID, a.clientSecret, a.realm)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.LoginClient")
+	}
+
+	return a.client.ExecuteActionsEmail(ctx, adminToken.AccessToken, a.realm, gocloak.ExecuteActionsEmail{
+		UserID:   gocloak.StringP(userID),
+		ClientID: gocloak.StringP(a.clientID),
+		Actions:  &([]string{"UPDATE_PASSWORD"}),
+	})
 }
 
 // RefreshToken implements pkg.Auth.
@@ -85,7 +133,8 @@ func (a *auth) RefreshToken(ctx context.Context, userID string, refreshToken str
 
 // ResetPassword implements pkg.Auth.
 func (a *auth) ResetPassword(ctx context.Context, reset *pkg.ResetPasswordRequest) error {
-	panic("unimplemented")
+	// TODO: implement after
+	return nil
 }
 
 // SignIn implements pkg.Auth.
@@ -113,7 +162,7 @@ func (a *auth) SignIn(ctx context.Context, payload *pkg.SignInRequest) (*pkg.Use
 
 // SignOut implements pkg.Auth.
 func (a *auth) SignOut(ctx context.Context, userID string, refreshToken string) error {
-	panic("unimplemented")
+	return a.client.Logout(ctx, a.clientID, a.clientSecret, a.realm, refreshToken)
 }
 
 // SignUp implements pkg.Auth.
@@ -136,6 +185,7 @@ func (a *auth) SignUp(ctx context.Context, payload *pkg.SignUpRequest) (string, 
 
 	attributes := make(map[string][]string)
 
+	// kha nang cao error
 	if payload.Phone != "" {
 		attributes["phoneNumber"] = []string{payload.Phone}
 	}
@@ -187,8 +237,81 @@ func (a *auth) SignUp(ctx context.Context, payload *pkg.SignUpRequest) (string, 
 }
 
 // UpdateUser implements pkg.Auth.
-func (a *auth) UpdateUser(ctx context.Context, accessToken string, user *pkg.UpdateUserRequest) error {
-	panic("unimplemented")
+func (a *auth) UpdateUser(ctx context.Context, accessToken string, payload *pkg.UpdateUserRequest) error {
+	// get admin token for updating the user
+	adminToken, err := a.client.LoginClient(ctx, a.clientID, a.clientSecret, a.realm)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.LoginClient")
+	}
+
+	// get user to set new fields to update
+	user, err := a.client.GetUserByID(ctx, adminToken.AccessToken, a.realm, payload.ID)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.GetUserByID")
+	}
+
+	if user.Attributes == nil {
+		user.Attributes = &map[string][]string{}
+	}
+
+	// set user fields to update
+
+	if payload.FirstName != nil {
+		user.FirstName = payload.FirstName
+	}
+
+	if payload.LastName != nil {
+		user.LastName = payload.LastName
+	}
+
+	var emailChanged = false
+
+	if payload.Email != nil {
+		if gocloak.PString(user.Email) != gocloak.PString(payload.Email) {
+			emailChanged = true
+			user.EmailVerified = gocloak.BoolP(false)
+			user.Email = payload.Email
+		}
+	}
+
+	if payload.Phone != nil {
+		(*user.Attributes)["phoneNumber"] = []string{gocloak.PString(payload.Phone)}
+	}
+
+	if payload.BirthDate != nil {
+		(*user.Attributes)["birthDate"] = []string{gocloak.PString(payload.BirthDate)}
+	}
+
+	if payload.NotifToken != nil {
+		(*user.Attributes)["notifToken"] = []string{gocloak.PString(payload.NotifToken)}
+	}
+
+	// update the user
+	err = a.client.UpdateUser(ctx, accessToken, a.realm, *user)
+
+	if err != nil {
+		return errors.Wrap(err, "a.client.UpdateUser")
+	}
+
+	// send email verification
+
+	if emailChanged {
+		err = a.client.LogoutAllSessions(ctx, adminToken.AccessToken, a.realm, payload.ID)
+
+		if err != nil {
+			return errors.Wrap(err, "a.client.LogoutAllSessions")
+		}
+
+		err = a.client.SendVerifyEmail(ctx, adminToken.AccessToken, payload.ID, a.realm)
+
+		if err != nil {
+			return errors.Wrap(err, "a.client.SendVerifyEmail")
+		}
+	}
+
+	return nil
 }
 
 // helper function to get user
